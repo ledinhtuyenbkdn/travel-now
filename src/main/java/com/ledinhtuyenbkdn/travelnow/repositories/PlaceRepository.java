15c102f700beef64557066f1e416327b3db0d97f
ledinhtuyenbkdn <ledinhtuyenbkdn@gmail.com>
2019-03-14 15:03:38 +0700

    adding StorageService interface. Closes #10

--- src/main/java/com/ledinhtuyenbkdn/travelnow/repositories/PlaceRepository.java
+++ src/main/java/com/ledinhtuyenbkdn/travelnow/repositories/PlaceRepository.java
@@ -4,4 +4,5 @@ import com.ledinhtuyenbkdn.travelnow.model.Place;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 public interface PlaceRepository extends Neo4jRepository<Place, Long> {
+    Place findByNamePlace(String name);
 }
