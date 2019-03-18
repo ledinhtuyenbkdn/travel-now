2ce51f5bb3c47664f62da2247d6f0abd0f9f1167
ledinhtuyenbkdn <ledinhtuyenbkdn@gmail.com>
2019-03-18 21:46:38 +0700

    implement Dropbox Storage Service. Closes #17

---
+++ src/main/java/com/ledinhtuyenbkdn/travelnow/services/StorageServiceDropboxImpl.java
@@ -0,0 +1,50 @@
+package com.ledinhtuyenbkdn.travelnow.services;
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class StorageServiceDropboxImpl implements StorageService {
+    private DbxClientV2 dbxClientV2;
+
+    @Autowired
+    public StorageServiceDropboxImpl(DbxClientV2 dbxClientV2) {
+        this.dbxClientV2 = dbxClientV2;
+    }
+
+    @Override
+    public void store(String fileClientAddress, String filename) {
+        try (InputStream in = new FileInputStream(fileClientAddress)) {
+            FileMetadata metadata = dbxClientV2.files().uploadBuilder("/" + filename)
+                    .uploadAndFinish(in);
+        } catch (DbxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(String filename) {
+        try {
+            dbxClientV2.files().deleteV2("/" + filename);
+        } catch (DbxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String load(String filename) {
+        try {
+            return dbxClientV2.files().getTemporaryLink("/" + filename).getLink();
+        } catch (DbxException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
