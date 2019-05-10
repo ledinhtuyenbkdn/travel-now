51cdda84d4040273a8b67ab01700fd8dd160197d
ledinhtuyenbkdn <ledinhtuyenbkdn@gmail.com>
2019-05-10 21:28:30 +0700

    fix: sort newsfeed, post by updated time

--- src/main/java/com/ledinhtuyenbkdn/travelnow/repository/PostRepository.java
+++ src/main/java/com/ledinhtuyenbkdn/travelnow/repository/PostRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import java.util.List;
 
 public interface PostRepository extends Neo4jRepository<Post, Long> {
-    @Query("match (i1:Image)<-[r1:HAS_IMAGE]-(t:Tourist)<-[r2:POSTED_BY]-(p:Post)-[r3:HAS_IMAGE]->(i2:Image), (p:Post)-[r4:TAG_WITH]->(place:Place)-[r5:HAS_IMAGE]->(i3:Image) where ID(t) = {0} return r1,r2,r3,r4,r5,p,place,i1,i2,i3,t;")
+    @Query("match (i1:Image)<-[r1:HAS_IMAGE]-(t:Tourist)<-[r2:POSTED_BY]-(p:Post)-[r3:HAS_IMAGE]->(i2:Image), (p:Post)-[r4:TAG_WITH]->(place:Place)-[r5:HAS_IMAGE]->(i3:Image) where ID(t) = {0} return r1,r2,r3,r4,r5,p,place,i1,i2,i3,t order by p.updatedAt desc;")
     List<Post> findAllByTouristId(Long id);
 
-    @Query("match (i1:Image)<-[r1:HAS_IMAGE]-(t:Tourist)<-[r2:POSTED_BY]-(p:Post)-[r3:HAS_IMAGE]->(i2:Image), (p:Post)-[r4:TAG_WITH]->(place:Place)-[r5:HAS_IMAGE]->(i3:Image), (you:Tourist)-[r6:IS_FOLLOWING]->(t:Tourist) where ID(you) = {0} return r1,r2,r3,r4,r5,p,place,i1,i2,i3,t;")
+    @Query("match (i1:Image)<-[r1:HAS_IMAGE]-(t:Tourist)<-[r2:POSTED_BY]-(p:Post)-[r3:HAS_IMAGE]->(i2:Image), (p:Post)-[r4:TAG_WITH]->(place:Place)-[r5:HAS_IMAGE]->(i3:Image), (you:Tourist)-[r6:IS_FOLLOWING]->(t:Tourist) where ID(you) = {0} return r1,r2,r3,r4,r5,p,place,i1,i2,i3,t order by p.updatedAt desc;")
     List<Post> findNewsFeed(Long id);
 }
